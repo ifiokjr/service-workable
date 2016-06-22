3fa9c4667cc2a05b9cde87f05df0a34b974a1e68
Ifiok Jr <ifiokotung@gmail.com>
2016-06-22 12:37:42 +0100

    update to cache and saving for later
    nothing still works, just working from two different computers.

--- src/cache.js
+++ src/cache.js
@@ -1,4 +1,51 @@
-import { install$ } from './streams'
+import { install$, fetch$, activate$ } from './streams'
+import debug from 'debug'
+
+const log = debug('sw:cache')
+const CACHE_NAME = 'deps'
+
+
+function openCaches() {
+  caches.open(CACHE_NAME)
+    .then(cache =>
+      fetch('files-to-cache.json')
+        .then(response =>
+          response.json()
+        ).then(files => {
+          log(`[openCaches] adding files from json file: ${files}`)
+          return cache.addAll(files)
+        })
+    ).then(() => {
+      log('[openCaches] All required resources have been cached and the installation successful')
+      return self.skipWaiting()
+    })
+}
+
+function matchCaches(event) {
+  caches.match(event.request)
+    .then(response => {
+      if (response) {
+        log(`[fetch] returning from service worker cache: ${event.request.url}`)
+        return response
+      }
+      log(`[fetch] returning from the server ${event.request.url}`)
+      return fetch(event.request)
+    })
+}
+
 
 install$
-  .map(event => event)
+  .subscribe(event => {
+    log('[install] kicking off service worker registration')
+    event.waitUntil(openCaches())
+  })
+
+fetch$
+  .subscribe(event =>
+    event.respondWith(matchCaches(event))
+  )
+
+activate$
+  .subscribe(event =>
+    event.waitUntil(self.clients.claim())
+  )
